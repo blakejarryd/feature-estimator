f265f6cc4926d2eecb212f0c109e7e0beb7208ad
blakejarryd <106789085+blakejarryd@users.noreply.github.com>
2024-12-06 14:04:40 +1100

    Add project selector component and update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { ProjectSelector } from '../components/project-selector';
 import { FeatureEstimator } from '../components/feature-estimator';
 import { EffortConfiguration } from '../components/effort-config';
 
 export default function Home() {
   return (
     <div className="space-y-8 p-8">
+      <ProjectSelector />
       <FeatureEstimator />
       <EffortConfiguration />
     </div>
